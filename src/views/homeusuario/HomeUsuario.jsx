7fd2b4b08c073c63341896028d126f5e56c67390
aluno <aluno@ifpe>
2023-12-07 16:28:43 -0300

    imagens

---
+++ src/views/homeusuario/HomeUsuario.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Image, Menu} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+
+export default function HomeUsuario() {
+    return (
+               
+           <div>
+            <Menu fixed="top" inverted style={{ background: '#ed9134', display: 'flex', justifyContent: 'center' }}>
+                <Menu.Item as={Link} to="/">Inicio</Menu.Item>
+                <Menu.Item as={Link} to="/tela-empresa">EMPRESA</Menu.Item>
+                <Menu.Item as={Link} to="/pontos-de-coleta">PONTOS DE COLETA</Menu.Item>
+                <Menu.Item as={Link} to="/sobre-nos">SOBRE NÓS</Menu.Item>
+            </Menu>
+
+           
+            <Container fluid style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 0 }}>
+
+                
+                {/* Div 1 */}
+                <div style={{ width: '100%',  marginBottom: '0px', background: '#e6f7ff' }}>
+                    <Image src='imagemtelahome.jpg' fluid />               
+                </div>  
+
+               {/* Div 2 */}
+      <div style={{ width: '100%', overflowX: 'auto', whiteSpace: 'nowrap', marginBottom: '20px', display: 'flex', flexDirection: 'row', alignItems: 'center', }}>   
+        <div style={{ minWidth: '100%', padding: '70px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{ marginTop: '20px', textAlign: 'center' }}>
+            <div>
+                <h2><b></b></h2>
+                <p></p>
+            </div>
+            <br></br>
+            <div>
+                <h2></h2>
+                <p>Nosso objetivo é conscientizar e mostrar opções seguras sobre o descarte de lixo eletrônico.</p>
+            </div>
+        </div>
+    </div>   
+    </div>
+
+{/* Div 3 */}
+<div style={{ width: '100%',  padding: '80px', marginBottom: '20px', background: '#008080', textAlign: 'center' }}>    
+    <h2>EMPRESAS PARCEIRAS:</h2> 
+    <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+        <div style={{}}>
+            <Image src='ambipar.png' size='small' centered circular />    
+        </div>
+        <div>
+            <Image src='celpe.png' size='small' centered circular bordered/>           
+        </div>
+        <div>
+            <Image src='duracell.png' size='small' centered circular />          
+        </div>
+        <div>
+            <Image src='atacadao.png' size="small" centered circular />          
+        </div>
+    </div>
+</div>
+            </Container>
+       </div>
+    )
+}
